7f8f0931abfa88fdf96edb5f145681d6f2e2f096
Sasi_Vardhan_Vemuri <163669536+Sasi-Vardhan@users.noreply.github.com>
2025-06-19 11:55:16 +0530

    Update firestore_config.py

--- firestore_config.py
+++ firestore_config.py
@@ -3,7 +3,21 @@ import firebase_admin
 from firebase_admin import credentials,firestore
 import os,json
 
+
+# Load JSON from environment variable
+firebase_json = os.environ.get("FIREBASE_CREDENTIALS_JSON")
+
+# Convert JSON string to dictionary
+cred_dict = json.loads(firebase_json)
+
 # cred_dict = json.loads(os.getenv("FIREBASE_CRED_JSON"))
-cred = credentials.Certificate("authenticationf-db6f1-firebase-adminsdk-fbsvc-942685adc5.json")
+
+cred = credentials.Certificate(cred_dict)
 firebase_admin.initialize_app(cred)
+
+# Create Firestore client
 db = firestore.client()
+
+# cred = credentials.Certificate("authenticationf-db6f1-firebase-adminsdk-fbsvc-942685adc5.json")
+# firebase_admin.initialize_app(cred)
+# db = firestore.client()
